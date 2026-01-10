a55855147b56c8bf155c386c9beb690dbeb06f3b
pavibur <test@DESKTOP-FO98OPP>
2026-01-10 13:18:59 +0100

    Update tb with diferrent freq and delay

--- run.py
+++ run.py
@@ -9,4 +9,38 @@ lib.add_source_files("./design/*.vhd")
 tb_lib = vu.add_library("tb_lib")
 tb_lib.add_source_files("./testbench/*.vhd")
 
+tb = tb_lib.test_bench("tb_timer")
+
+tb.add_config(
+    name="f10Hz_d1s",
+    generics={
+        "tb_clk_freq_g": 10,
+        "tb_delay_g": 1000
+    },
+)
+
+tb.add_config(
+    name="f10Hz_d2s",
+    generics={
+        "tb_clk_freq_g": 10,
+        "tb_delay_g": 2000
+    },
+)
+
+tb.add_config(
+    name="f20Hz_d500ms",
+    generics={
+        "tb_clk_freq_g": 20,
+        "tb_delay_g": 1000
+    },
+)
+
+tb.add_config(
+    name="f35Hz_d2600ms",
+    generics={
+        "tb_clk_freq_g": 35,
+        "tb_delay_g": 2600
+    },
+)
+
 vu.main()
--- testbench/tb_timer.vhd
+++ testbench/tb_timer.vhd
@@ -8,20 +8,26 @@ context vunit_lib.vunit_context;
 library design_lib;
 
 entity tb_timer is
-  generic (runner_cfg : string);
+  generic (
+    runner_cfg : string;
+    tb_clk_freq_g : natural := 10;
+    tb_delay_g : natural := 1000 
+    );
+  
 end entity;
 
 architecture tb of tb_timer is
   signal clk, reset_n, done : std_logic := '0';
   signal start : std_logic := '0';
-  constant clk_freq_hz_c : natural := 10;
-  constant clk_period_c  : time    := (1_000_000_000 / clk_freq_hz_c) * 1 ns;
+  constant clk_period_c  : time    := (1_000_000_000 / tb_clk_freq_g) * 1 ns;
+  signal clk_freq_hz_g_in : natural := tb_clk_freq_g;
+  signal delay_g_in : time := tb_delay_g * 1 ms;
 
 begin
   dut: entity design_lib.timer(rtl)
     generic map(
-      clk_freq_hz_g => 10,
-      delay_g       => 1000 ms
+      clk_freq_hz_g => clk_freq_hz_g_in,
+      delay_g       => delay_g_in
     )
     port map (
       clk_i     => clk,
@@ -44,7 +50,9 @@ begin
         start <= '1'; wait for clk_period_c;
         start <= '0';
         
-        wait until done = '1';
+        wait for delay_g_in - 100 ms;
+        check_equal(done, '0', "done should be 0 before delay");  
+        wait for delay_g_in;
         check_equal(done, '1', "done should assert after delay");  
         
       end if;
