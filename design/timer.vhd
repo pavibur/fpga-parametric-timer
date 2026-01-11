a1d56e997c062643840a53ac19c7e2c8f76cc02c
pavibur <test@DESKTOP-FO98OPP>
2026-01-11 18:12:32 +0100

    Try fix truncate problem

--- design/timer.vhd
+++ design/timer.vhd
@@ -17,27 +17,45 @@ entity timer is
 end entity timer ;
 
 architecture rtl of timer is
-  signal count :  unsigned(10 DOWNTO 0) := (OTHERS => '0');
+  type state_t is (IDLE, COUNTING);
+  signal state : state_t := IDLE;
+  
+  constant delay_fs_c     : real := real(delay_g / 1 fs);
+  constant freq_real_c    : real := real(clk_freq_hz_g);
+  constant cycles_real_c  : real := freq_real_c * delay_fs_c * 1.0e-15;
+  constant max_count_c       : natural := natural(ceil(cycles_real_c));
+
+  
+  
+  signal count :  unsigned(31 DOWNTO 0) := (OTHERS => '0');
   signal period_ns_c  : natural := 1_000_000_000 / clk_freq_hz_g;
-  signal max_count_c  : natural := natural(real(delay_g / 1 ns) / real(period_ns_c));
 begin
-  counting: process(clk_i, arst_i)
+  main: process(clk_i, arst_i)
   begin
     if arst_i = '1' then
       count    <= (OTHERS => '0');
       done_o   <= '1';
+      state <= IDLE;
     elsif rising_edge(clk_i) then
-      if done_o = '1' and start_i = '1' then  
-        count  <= to_unsigned(0, count'length);  
-        done_o <= '0';                           
-      elsif done_o = '0' then  
-        if count = max_count_c - 1 then
-          count  <= (others => '0');
-          done_o <= '1';  
-        else
-          count <= count + 1;
-        end if;
-      end if;  
+      case state is
+        when IDLE =>
+          done_o <= '1';
+          if start_i = '1' then
+            if max_count_c > 0 then
+              count   <= to_unsigned(max_count_c - 1, count'length);
+              state <= COUNTING;
+              done_o  <= '0';
+            end if;
+          end if;
+        when COUNTING =>
+          if count = 0 then
+            state <= IDLE;
+            done_o  <= '1';
+          else
+            count <= count - 1;
+            done_o <= '0';
+          end if;
+      end case;
     end if;
   end process;
 end architecture;
